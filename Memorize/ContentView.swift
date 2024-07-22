4ce9f5d462cccfc88e70cb4d001c0adc8d2f9ea4
Kiran <bokka.saikiranreddy@gmail.com>
2024-07-21 22:51:40 -0500

    feat: added card view

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,10 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("🚙")
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.green)
         .padding()
+        
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 15)
+                    .strokeBorder(lineWidth: 5)
+                Text("🚙")
+                    .font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 15)
+            }
+        }
     }
 }
 
