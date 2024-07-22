508dec321a80d94d547bb191cb30e4022b65b966
Kiran <bokka.saikiranreddy@gmail.com>
2024-07-21 22:25:15 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sai Kiran Reddy B on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
