dcdaefe801a44859425f9b50b321ecfb6dcd30b8
Kiran <bokka.saikiranreddy@gmail.com>
2024-07-22 19:46:31 -0500

    code refactor

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,24 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["🚙","😎","🚙","😎"]
+    let emojis = ["🚙","😎","👂🏻","🕷️","💍","🐸","🔥","⚽️"]
+    @State var cardCount: Int = 3
     var body: some View {
-            HStack{
-                ForEach(emojis.indices, id:\.self) { index in
-                    CardView(content: emojis[index])
-                }
-            }
-
-    
-        .foregroundColor(.green)
+        VStack{
+            cards
+            cardCountAdjusters
+        }
         .padding()
-        
+    }
+    
+    var cardCountAdjusters: some View{
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+        }.imageScale(.large)
+    }
+    
+    var cards: some View {
+        HStack{
+            ForEach(0..<cardCount, id:\.self) { index in
+                CardView(content: emojis[index])
+            }
+        }.foregroundColor(.green)
+    }
+    var cardRemover: some View {
+        Button(action: {
+            cardCount -= 1
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.minus")
+        })
+    }
+    
+    var cardAdder: some View {
+        Button(action: {
+            cardCount += 1
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.plus")
+        })
     }
 }
 
 struct CardView: View {
     let content: String
-    @State var isFaceUp = false
+    @State var isFaceUp = true
 
     var body: some View {
         ZStack {
